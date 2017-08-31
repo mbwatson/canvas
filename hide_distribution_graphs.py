24067a4dc8a317903326ebeb2682e2a0d06cb374
mwatson <mattwat@gmail.com>
2017-08-31 16:40:38 -0400

    refactor grab courses by term ids

--- hide_distribution_graphs.py
+++ hide_distribution_graphs.py
@@ -4,20 +4,28 @@
 from canvasapi import Canvas
 from itertools import chain
 
-# Get Canvas API URL and Key from user...
+def get_courses_by_term_ids(idList):
+	# Grab courses
+	courses = []
+	for termId in idList:
+		courses = list(chain(courses, account.get_courses(per_page=500, enrollment_term_id=termId)))
+	return(courses)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Pull credentials from json config file
+import json
+with open('config.json', 'r') as f:
+  config = json.load(f)
+API_URL = config['Production']['API_URL']
+API_KEY = config['Production']['API_KEY']
+# ...or prompt user for Canvas API URL and Key.
 print("\nEnter your Canvas ( _________.instructure.com ) : ")
 instance = input(" >> ")
 API_URL = "https://{}.instructure.com/api/v1/".format(instance)
 print("\nAPI KEY : ")
 API_KEY = input(" >> ")
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# ...or uncomment these lines to pull from json config file
-# import json
-# with open('config.json', 'r') as f:
-#   config = json.load(f)
-# API_URL = config['Production']['API_URL']
-# API_KEY = config['Production']['API_KEY']
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 # Attempt with entered credentials
 print("\nAttempting to access {}".format(API_URL))
 print("with API key {}...\n".format(API_KEY))
@@ -30,15 +38,10 @@ except:
 	print(" >> An error occurred!\n")
 else:
 	print("\n=== " + account.name + " ===\n")
-	# Gather courses for various current terms
-	fallCourses = account.get_courses(per_page=500, enrollment_term_id=101)
-	springCourses = account.get_courses(per_page=500, enrollment_term_id=105)
-	yearlongCourses = account.get_courses(per_page=500, enrollment_term_id=120)
-	# Combine all current courses into one list
-	courses = list(chain(fallCourses, springCourses, yearlongCourses))
+	courses = get_courses_by_term_ids([101, 105, 120])
 	# Iterate through each course, updating settings
 	for course in courses:
-		print(course.name + " ( " + str(course.id) + " )")
-		# course.update_settings(hide_distribution_graphs="true")
+		print(course.name + " (id=" + str(course.id) + ", term_id=" + str(course.enrollment_term_id) + ")")
+		course.update_settings(hide_distribution_graphs="true")
 		print("OK!")
 
