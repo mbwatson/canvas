c2802d8bf347b7e4d7f46fbeac1fdd43b4e34059
mbwatson <mattwat@gmail.com>
2017-09-14 18:52:18 -0400

    add ability to give command line arguments for term ids

--- update_observers_notification_preferences.py
+++ update_observers_notification_preferences.py
@@ -5,6 +5,7 @@ from itertools import chain
 import requests
 import json
 import time
+import sys
 
 program_start_time = time.time()
 
@@ -19,12 +20,17 @@ headers = {
 }
 
 # Ids of terms from which to gather courses and their observers
-term_ids = [101, 105, 120] # IDs of terms from which to gather obervers
+# Enter term_ids at program execution:
+# "python3 update_observers_notification_preferences.py 101,105,120"
+term_ids = sys.argv[1].split(',')
+
+def time_since(old_time):
+    return time.time() - old_time
 
 def get_courses_by_term_ids(term_ids):
 	courses = []
-	for termId in term_ids:
-		courses = list(chain(courses, account.get_courses(per_page=500, enrollment_term_id=termId)))
+	for term_id in term_ids:
+		courses = list(chain(courses, account.get_courses(per_page=500, enrollment_term_id=term_id)))
 	return(courses)
 
 def get_course_observer_ids(course):
@@ -57,13 +63,17 @@ except:
 	print("An error occurred accessing the API!")
 else:
 	all_observer_ids = []
+	print("Accessing Term IDs:")
+	for term_id in term_ids:
+		print(" " + str(term_id))
+	print()
 	courses = get_courses_by_term_ids(term_ids)
 	print("Courses\n" + "="*60)
 	# Loop through all the courses
 	for course in courses:
 		course_start_time = time.time()
 		course_observer_ids = get_course_observer_ids(course)
-		print(course.name + " + " + str(len(course_observer_ids)) + " observers")
+		print(course.name + "  +" + str(len(course_observer_ids)) + " observers")
 		all_observer_ids = all_observer_ids + course_observer_ids
 	# Remove duplicates/convert to set
 	all_observer_ids = set(all_observer_ids)
@@ -80,7 +90,7 @@ else:
 		print(" - " + user.name + " (ID: " + str(user.id) + ")\n" + "-"*60)
 		update_user_notification_preferences(user, "never")
 		print()
-		print("User change time: {} seconds".format(time.time() - user_start_time))
-		print("Total runtime: {} seconds".format(time.time() - program_start_time))
+		print("User change time: {} seconds".format(time_since(user_start_time)))
+		print("Total runtime: {} seconds".format(time_since(program_start_time)))
 		print()
 		print("="*60)
