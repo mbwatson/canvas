1fea563c981b8869b8ae74db5088c3ff1ef0ac5d
mwatson <mattwat@gmail.com>
2017-09-15 16:31:25 -0400

    add two dim'l array for gradebook

--- show_outcomes.py
+++ show_outcomes.py
@@ -7,8 +7,8 @@ import json
 import time
 import sys
 
-outcome_ids = [5420,5419,5418,5417,5416,5422]
-# ,5421,5423,5424,5425,5426,5435,5434,5433,5432,5431,5430,5429,5428,5427,5441,5451,5450,5449,5448,5447,5440,5439,5438,5446,5445,5444,5443,5442,5437,5436,5415,5414,5411,5412,5413,5405,5406,5407,5408,5409,5410]
+outcome_ids = [5405,5406,5407,5408,5409,5410]
+# ,5420,5419,5418,5417,5416,5422,5421,5423,5424,5425,5426,5435,5434,5433,5432,5431,5430,5429,5428,5427,5441,5451,5450,5449,5448,5447,5440,5439,5438,5446,5445,5444,5443,5442,5437,5436,5415,5414,5411,5412,5413]
 
 def time_since(old_time):
     return time.time() - old_time
@@ -52,10 +52,20 @@ else:
 	course = canvas.get_course(1875)
 	print(course.name)
 	students = course.get_users(enrollment_type="student")
+	gradebook = [[0 for col in range(len(list(outcomes)))] for row in range(len(list(students)))]
+	student_index = 0
+	outcome_index = 0
 	for student in students:
+		outcome_index = 0
 		print(student.name)
 		for outcome in outcomes:
 			response = requests.get(API_URL + "courses/{}/outcome_results?user_ids[]={}".format(course.id, student.id), headers = headers)
-			print("  {}".format(outcome))
-			outcome_results = response.json()['outcome_results']
-			print(outcome_results)
+			score = response.json()['outcome_results'][0]['score']
+			print("  {}: {}".format(outcome, score))
+			print(student_index)
+			print(outcome_index)
+			gradebook[student_index][outcome_index] = score
+			outcome_index += 1
+		print()
+		student_index += 1
+	print(gradebook)
