403f29fc3010f88d11815d89263c4def72a46de0
mwatson <mattwat@gmail.com>
2017-09-20 23:02:07 -0400

    text format display

--- show_outcomes.py
+++ show_outcomes.py
@@ -7,8 +7,7 @@ import json
 import time
 import sys
 
-outcome_ids = [5405,5406,5407,5408,5409,5410]
-# ,5420,5419,5418,5417,5416,5422,5421,5423,5424,5425,5426,5435,5434,5433,5432,5431,5430,5429,5428,5427,5441,5451,5450,5449,5448,5447,5440,5439,5438,5446,5445,5444,5443,5442,5437,5436,5415,5414,5411,5412,5413]
+outcome_ids = [5405,5406,5407,5408,5409,5410,5420,5419,5418,5417,5416,5422,5421,5423,5424,5425,5426,5435,5434,5433,5432,5431,5430,5429,5428,5427,5441,5451,5450,5449,5448,5447,5440,5439,5438,5446,5445,5444,5443,5442,5437,5436,5415,5414,5411,5412,5413]
 
 def time_since(old_time):
     return time.time() - old_time
@@ -26,8 +25,8 @@ program_start_time = time.time()
 # Read login credentials
 with open('config.json', 'r') as f:
   config = json.load(f)
-API_URL = config['Beta']['API_URL']
-API_KEY = config['Beta']['API_KEY']
+API_URL = config['Production']['API_URL']
+API_KEY = config['Production']['API_KEY']
 headers = {
     'Content-type': 'application/json',
     'Authorization' : 'Bearer ' + API_KEY
@@ -57,13 +56,11 @@ else:
 	outcome_index = 0
 	for student in students:
 		outcome_index = 0
-		print(student.name)
+		print("{} ({})".format(student.name, student.id))
 		for outcome in outcomes:
 			response = requests.get(API_URL + "courses/{}/outcome_results?user_ids[]={}".format(course.id, student.id), headers = headers)
 			score = response.json()['outcome_results'][0]['score']
 			print("  {}: {}".format(outcome, score))
-			print(student_index)
-			print(outcome_index)
 			gradebook[student_index][outcome_index] = score
 			outcome_index += 1
 		print()
